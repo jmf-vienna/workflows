e68f3de8578b36e53c2b76813edbb579f2d3fc0d
Jay Osvatic <jayosvatic@gmail.com>
2024-04-03 11:33:37 +0200

    added snakemake v8 script cleaned up directories

---
+++ workflow/scripts/runSnakemake_v8.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#SBATCH --job-name=run_snakemake
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+#SBATCH --output=snakemake.out
+#SBATCH --error=snakemake.err
+#SBATCH --time=30-00:00:00
+
+#module load miniconda
+module load snakemake/8.10.4-3.12.2
+module load conda
+
+snakemake --snakefile $1 --executor cluster-generic --cluster-generic-submit-cmd "sbatch --mem {resources.mem_mb}\
+                                                  --cpus-per-task {threads}\
+						  --partition={resources.partition}\
+                                                  --time={resources.time}\
+                                                  --output=log/slurm_{rule}-%A.out\
+                                                  --error=log/slurm_{rule}-%A.err"\
+                                --jobs 20\
+				--use-conda
+                                #--keep-going #-R diamond_search
+
+
